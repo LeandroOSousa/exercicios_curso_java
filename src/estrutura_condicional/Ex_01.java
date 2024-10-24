c92f5ff69368b170c8e6140ee692f931e46b3c2d
Leandro <lean.sousa@gmail.com>
2024-10-24 17:11:17 -0300

    exercicio 1 - estrutura condicional

--- src/estrutura_condicional/Ex_01.java
+++ src/estrutura_condicional/Ex_01.java
@@ -1,7 +1,25 @@
 package estrutura_condicional;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Ex_01 {
     public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        /*
+        Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+         */
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+        sc.close();
+
+        if (numero < 0) {
+            System.out.println("O número é negativo.");
+        }else {
+            System.out.println("O número não é positivo.");
+        }
     }
 }
